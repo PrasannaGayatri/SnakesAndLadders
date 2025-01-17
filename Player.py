a426127aaf5cbaa5d4c7d90983857e55c9e87a95
Gayatri V <166007267+PrasannaGayatri@users.noreply.github.com>
2025-01-17 16:35:06 +0530

    Added docstring to Player.py

--- Player.py
+++ Player.py
@@ -1,4 +1,12 @@
 class Player:
+	"""
+	Represents a player in the game of Snakes and Ladders.
+    
+    	Attributes:
+	        id (int): The unique identifier for the player.
+	        name (str): The name of the player.
+	        position (int): The current position of the player on the game board.
+	"""
 	def __init__(self,  id, name):
 		self.name = name
 		self.id = id
